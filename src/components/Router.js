916e5a159111cf07a0585f0df22613972026e32a
sshs0702 <rebornist@naver.com>
2021-01-20 12:25:17 +0900

    로그인 기능 구현 완료

---
+++ src/components/Router.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
+
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/Profile.js
@@ -0,0 +1,16 @@
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
+import React from "react";
+
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
+};
